05add0ae36cccc1cd7663492ac852ead7137d346
Adrien RICHARD <richardadrien0@gmail.com>
2019-10-30 21:32:54 +0100

    pointer lock

--- main.js
+++ main.js
@@ -1,52 +1,30 @@
-
-var setupcamera = function (scene) {
-    var camera = new BABYLON.UniversalCamera("mainCamera", new BABYLON.Vector3(0, 0, 0), scene);
-    camera.fov = 45;
-    camera.minZ = 0.01;
-    camera.maxZ = 1000;
-    camera.speed = 0.5;
-    camera.position = new BABYLON.Vector3(0, 5, 0);
-    // camera.rotation = new BABYLON.Vector3(Math.PI / 4, Math.PI, 0); //https://doc.babylonjs.com/resources/rotation_conventions
-    camera.attachControl(canvas, true);
-    camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
-    camera.applyGravity = true;
-    camera.checkCollisions = true;
-    return camera;
-}
-
-
-var generateTerrain = function () {
-
-    var data = [];
-    var simplex = new SimplexNoise();
-    for (let i = 0; i < 25; i++) {
-        data[i] = [];
-        for (let j = 0; j < 25; j++) {
-            data[i][j] = [];
-            data[i][j] = simplex.noise2D(i, j) * 10;
-        }
-    }
-    console.log(data);
-
-    for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j < data[i].length; j++) {
-            var ir = BABYLON.MeshBuilder.CreateBox("ir", {}, scene);
-            ir.position = new BABYLON.Vector3(i, Math.ceil(data[i][j]), j);
-            ir.receiveShadows = true;
-            ir.checkCollisions = true;
-        }
+//see : https://www.babylonjs-playground.com/#4P4FTN#1 
+// for pointer lock 
+//had to do this because the basic function was not waorking for some reason
+BABYLON.Engine.prototype.enterPointerlock = function () {
+    if (this._renderingCanvas) {
+        BABYLON.Tools.RequestPointerlock(this._renderingCanvas);
     }
 }
 
-var generateFlatTerrain = function () {
+BABYLON.Engine.prototype.exitPointerlock = function () {
+    BABYLON.Tools.ExitPointerlock();
+}
 
-    for (let i = 0; i < 25; i++) {
-        for (let j = 0; j < 25; j++) {
-            var ir = BABYLON.MeshBuilder.CreateBox("ir", {}, scene);
-            ir.position = new BABYLON.Vector3(i, 0, j);
-            ir.receiveShadows = true;
-            ir.checkCollisions = true;
-        }
+
+BABYLON.Tools.RequestPointerlock = function (element) {
+    element.requestPointerLock = element.requestPointerLock || (element).msRequestPointerLock || (element).mozRequestPointerLock || (element).webkitRequestPointerLock;
+    if (element.requestPointerLock) {
+        element.requestPointerLock();
+    }
+}
+
+BABYLON.Tools.ExitPointerlock = function (element) {
+    let anyDoc = document; // as any;
+    document.exitPointerLock = document.exitPointerLock || anyDoc.msExitPointerLock || anyDoc.mozExitPointerLock || anyDoc.webkitExitPointerLock;
+
+    if (document.exitPointerLock) {
+        document.exitPointerLock();
     }
 }
 
@@ -54,6 +32,7 @@ var canvas = document.getElementById("renderCanvas"); // Get the canvas element
 var engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
 var createScene = function () {
+    engine.enterPointerlock();
 
     var scene = new BABYLON.Scene(engine);
     scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
@@ -61,6 +40,8 @@ var createScene = function () {
 
     //  generateTerrain();
     generateFlatTerrain();
+    createGui();
+
     var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
 
     myMaterial.diffuseColor = new BABYLON.Color3(1, 0, 1);
@@ -69,10 +50,10 @@ var createScene = function () {
     //myMaterial.ambientColor = new BABYLON.Color3(0.23, 0.98, 0.53);
 
     // Add lights to the scene
-    var light1 = new BABYLON.PointLight("light1", new BABYLON.Vector3(0, 5, 0), scene);
+    var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 5, 0), scene);
     light1.intensity = 0.75;
-    var shadowGenerator = new BABYLON.ShadowGenerator(1048, light1);
-    shadowGenerator.useBlurExponentialShadowMap = true;
+    //var shadowGenerator = new BABYLON.ShadowGenerator(1048, light1);
+    // shadowGenerator.useBlurExponentialShadowMap = true;
 
     var camera = setupcamera(scene);
 
@@ -81,40 +62,135 @@ var createScene = function () {
     box.parent = camera;
 */
     //scene.debugLayer.show();
-   
-    function vecToLocal(vector, mesh){
+
+    function vecToLocal(vector, mesh) {
         var m = mesh.getWorldMatrix();
         var v = BABYLON.Vector3.TransformCoordinates(vector, m);
-		return v;		 
+        return v;
     }
 
-    function castRay(){       
+    function castRay() {
         var origin = camera.position;
-	
-	    var forward = new BABYLON.Vector3(0,0,1);		
-	    forward = vecToLocal(forward, camera);
-	
-	    var direction = forward.subtract(origin);
-	    direction = BABYLON.Vector3.Normalize(direction);
-	
-	    var length = 100;
-	
-	    var ray = new BABYLON.Ray(origin, direction, length);
 
-	//	let rayHelper = new BABYLON.RayHelper(ray);		
-	//	rayHelper.show(scene);		
+        var forward = new BABYLON.Vector3(0, 0, 1);
+        forward = vecToLocal(forward, camera);
+
+        var direction = forward.subtract(origin);
+        direction = BABYLON.Vector3.Normalize(direction);
+
+        var length = 100;
+
+        var ray = new BABYLON.Ray(origin, direction, length);
+
+        //	let rayHelper = new BABYLON.RayHelper(ray);		
+        //	rayHelper.show(scene);		
 
         var hit = scene.pickWithRay(ray);
 
-        if (hit.pickedMesh){
-           //hit.pickedMesh.scaling.y += 0.01;
-           hit.pickedMesh.material = myMaterial;
-	    }
+        if (hit.pickedMesh) {
+            hit.pickedMesh.material = myMaterial;
+        }
     }
+
+    function createGui() {
+        // GUI
+        var advancedTexture = new BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+        var createRectangle = function () {
+            var rect1 = new BABYLON.GUI.Rectangle();
+            rect1.width = 0.2;
+            rect1.height = "40px";
+            rect1.cornerRadius = 5;
+            rect1.color = "Orange";
+            rect1.thickness = 4;
+            rect1.background = "green";
+            advancedTexture.addControl(rect1);
+            return rect1;
+        }
+        createRectangle().verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+
+        var cross = new BABYLON.GUI.Rectangle();
+        cross.width = "20px";
+        cross.height = "2px";
+        cross.color = "black";
+        cross.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+        cross.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+        advancedTexture.addControl(cross);
+
+        var cross1 = new BABYLON.GUI.Rectangle();
+        cross1.width = "2px";
+        cross1.height = "20px";
+        cross1.color = "black";
+        cross1.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+        cross1.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+        advancedTexture.addControl(cross1);
+    }
+
+    function generateFlatTerrain() {
+        for (let i = 0; i < 25; i++) {
+            for (let j = 0; j < 25; j++) {
+                var ir = BABYLON.MeshBuilder.CreateBox("ir", {}, scene);
+                ir.position = new BABYLON.Vector3(i, 0, j);
+                ir.receiveShadows = true;
+                ir.checkCollisions = true;
+            }
+        }
+    }
+
+
+    function generateTerrain() {
+
+        var data = [];
+        var simplex = new SimplexNoise();
+        for (let i = 0; i < 25; i++) {
+            data[i] = [];
+            for (let j = 0; j < 25; j++) {
+                data[i][j] = [];
+                data[i][j] = simplex.noise2D(i, j) * 10;
+            }
+        }
+        console.log(data);
+
+        for (let i = 0; i < data.length; i++) {
+            for (let j = 0; j < data[i].length; j++) {
+                var ir = BABYLON.MeshBuilder.CreateBox("ir", {}, scene);
+                ir.position = new BABYLON.Vector3(i, Math.ceil(data[i][j]), j);
+                ir.receiveShadows = true;
+                ir.checkCollisions = true;
+            }
+        }
+    }
+
+
+    function setupcamera(scene) {
+        var camera = new BABYLON.UniversalCamera("mainCamera", new BABYLON.Vector3(0, 0, 0), scene);
+        camera.fov = 45;
+        camera.minZ = 0.01;
+        camera.maxZ = 1000;
+        camera.speed = 0.5;
+        camera.position = new BABYLON.Vector3(0, 5, 0);
+        // camera.rotation = new BABYLON.Vector3(Math.PI / 4, Math.PI, 0); //https://doc.babylonjs.com/resources/rotation_conventions
+        camera.attachControl(canvas, true);
+        camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+        camera.applyGravity = true;
+        camera.checkCollisions = true;
+        return camera;
+    }
+
 
     scene.registerBeforeRender(function () {
         castRay();
     });
+
+
+    scene.onPointerUp = function () {
+        if (!document.pointerLockElement) {
+            engine.enterPointerlock();
+        }
+        else {
+            engine.exitPointerlock();
+        }
+    }
 
     return scene;
 };
@@ -139,5 +215,5 @@ window.addEventListener("click", function () {
     var pickResult = scene.pick(scene.pointerX, scene.pointerY);
     console.log(pickResult);
     pickResult.originMesh.material = myMaterial;
-    
+
  });*/
