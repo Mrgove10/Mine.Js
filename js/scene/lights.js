974155a3e123e95d8f0ec1bc64a121d2530f19e4
Adrien RICHARD <richardadrien0@gmail.com>
2019-11-03 13:36:12 +0100

    fixed shadows

--- js/scene/lights.js
+++ js/scene/lights.js
@@ -1,8 +1,14 @@
 export function setupLights(scene) {
     scene.clearColor = BABYLON.Color3.FromHexString("#80EBFF")
     //    scene.fogMode = BABYLON.Scene.FOGMODE_EXP;
-    var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 5, 0), scene);
-    light1.intensity = 0.75;
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 5, 0), scene);
+    light.diffuse = new BABYLON.Color3(0.75, 0.75, 0.75);
+	light.specular = new BABYLON.Color3(0.75, 0.75, 0.75);
+    light.groundColor = new BABYLON.Color3(0.75, 0.75, 0.75);    
+    
+   // light1.intensity = 0.75;
+    var light2 = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(0, -1, 0), scene);
+  //  light2.intensity = 0.75;
     //var shadowGenerator = new BABYLON.ShadowG enerator(1048, light1);
     // shadowGenerator.useBlurExponentialShadowMap = true;
 }
